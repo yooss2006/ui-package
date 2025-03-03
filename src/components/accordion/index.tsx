8cc8e0fc1d01331a96e02c891134bfeee920fbe7
yooss2006 <yooss2006@naver.com>
2025-03-03 16:06:12 +0900

    Feat:[Accordion] JS 구현

--- src/components/accordion/index.tsx
+++ src/components/accordion/index.tsx
@@ -1,6 +1,7 @@
 import Accordion1 from "./1_r";
 import Accordion2 from "./2_r";
 import Accordion3 from "./3_r";
+import Accordion4V from "./4_v";
 import cx from "./cx";
 
 const Accordions = () => {
@@ -10,6 +11,7 @@ const Accordions = () => {
             <Accordion1 />
             <Accordion2 />
             <Accordion3 />
+            <Accordion4V />
         </div>
     );
 }
