fc9c79252d62baa722e68bc42acb51da2ca901a8
yooss2006 <yooss2006@naver.com>
2025-03-05 23:32:29 +0900

    Feat:[Tooltip] React - html 태그로 툴팁 구현

--- src/components/tooltip/index.tsx
+++ src/components/tooltip/index.tsx
@@ -1,5 +1,6 @@
 import Tooltip1 from "./1_r";
 import Tooltip2 from "./2_r";
+import Tooltip3 from "./3_r";
 import cx from "./cx";
 
 const ToolTips = () => {
@@ -8,6 +9,7 @@ const ToolTips = () => {
       <h2>툴팁</h2>
       <Tooltip1 />
       <Tooltip2 />
+      <Tooltip3 />
     </div>
   );
 };
