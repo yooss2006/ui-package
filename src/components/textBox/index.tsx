629e55af572ced6f3d7d03aaf97b48d891a2432a
yooss2006 <yooss2006@naver.com>
2025-03-06 22:53:38 +0900

    Feat:[TextBox] React - 숨겨둔 textarea Uncontrolled 방식 구현

--- src/components/textBox/index.tsx
+++ src/components/textBox/index.tsx
@@ -1,6 +1,7 @@
 import TextBox1 from "@/components/textBox/1_r";
 import cx from "./cx";
 import TextBox2 from "@/components/textBox/2_r";
+import TextBox3 from "@/components/textBox/3_r";
 
 const TextBoxes = () => {
   return (
@@ -8,6 +9,7 @@ const TextBoxes = () => {
       <h2>텍스트박스</h2>
       <TextBox1 />
       <TextBox2 />
+      <TextBox3 />
     </div>
   );
 };
